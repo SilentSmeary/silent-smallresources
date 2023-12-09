30de9a6ca30b3c6a921bf852864bf30a41803c49
James <102177366+SilentSmeary@users.noreply.github.com>
2023-12-09 11:38:20 +0000

    remove: fuel

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,12 +6,8 @@ description 'silent-smallresources'
 
 shared_scripts {
 -- External Scripts
-    '@qb-core/shared/locale.lua',
-    '@ox_lib/init.lua'
+    '@ox_lib/init.lua',
 -- Interal Scripts
-    -- Fuel
-    'fuel/shared/config.lua',
-    'fuel/locales/en.lua',
 }
 
 client_scripts {
@@ -23,36 +19,18 @@ client_scripts {
     'knockout/config.lua',
     -- Weapon Damage
     'weapon_damage/client.lua',
-    'weapon_damage/config.lua',
-    -- Fuel
-    'fuel/client/fuel_cl.lua',
-    'fuel/client/electric_cl.lua',
-    'fuel/client/station_cl.lua',
-    'fuel/client/utils.lua'
+    'weapon_damage/config.lua'
 }
 
 server_scripts {
 -- External Scripts
     '@oxmysql/lib/MySQL.lua',
 -- Interal Scripts
-    -- Fuel
-    'fuel/server/fuel_sv.lua',
-    'fuel/server/station_sv.lua',
-    'fuel/server/electric_sv.lua',
 }
 
 lua54 'yes'
 
-exports {
-    'GetFuel',
-    'SetFuel'
-}
 
 dependencies {
     'ox_lib',
 }
-
-data_file 'DLC_ITYP_REQUEST' 'stream/[electric_nozzle]/electric_nozzle_typ.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/[electric_charger]/electric_charger_typ.ytyp'
-
-provide 'cdn-syphoning'
